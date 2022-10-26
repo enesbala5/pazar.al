16c757ead9abdb32b9c93c928551cc42961fdb36
Enes Bala <47826227+enesbala5@users.noreply.github.com>
2022-10-26 12:59:43 +0200

    Hopefully fixed Vercel Bug - not deploying

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,28 +1,24 @@
 <script context="module">
-  export function load({ error, status }) {
-    return { props: { error, status } };
-  }
+	export function load({ error, status }) {
+		return { props: { error, status } };
+	}
 </script>
 
 <script>
-
 	// Initializations & Exports:
 	export let error;
 	export let status;
-	
 </script>
 
 <!-- #################################################################################################### -->
 
 <!-- Error Message -->
 <section>
-	<span></span>
+	<p>{error}</p>
+	<p>{status}</p>
 </section>
 
 <!-- #################################################################################################### -->
-
 <style>
-
 	/* CSS Goes Here */
-
 </style>
