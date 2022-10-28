4f0bf509d58b5c289c0d5265ccf0045e964b3d3a
Enes Bala <47826227+enesbala5@users.noreply.github.com>
2022-10-28 23:03:23 +0200

    real data now

--- src/routes/api/getLatestPosts/+server.ts
+++ src/routes/api/getLatestPosts/+server.ts
@@ -33,144 +33,29 @@ export const POST: RequestHandler = async ({ request }) => {
 
 	let keywords: string[] = getKeywords(query.id);
 
-	// let data = await prisma.post.findMany({
-	// 	take: itemsPerPage,
-	// 	skip: itemsToSkip,
-	// 	where: {
-	// 		OR: [
-	// 			{
-	// 				title: {
-	// 					contains: query.id,
-	// 				},
-	// 			},
-	// 			{
-	// 				title: {
-	// 					in: keywords,
-	// 				},
-	// 			},
-	// 		],
-	// 		AND: [
-	// 			{
-	// 				archived: false,
-	// 			},
-	// 		],
-	// 	},
-	// });
-
-	let data = [
-		{
-			pid: 'cl9oes2af00008iwogax8m0qk',
-			created_at: '2022-10-25T16:14:59.150Z',
-			archived: false,
-			post_views: 1,
-			post_likes: 1,
-			title: 'Range Rover 2022',
-			description: 'Super mjet 1',
-			price: 1,
-			eur: true,
-			old_price: 100,
-			city: 'Tirana',
-			country: 'Albania',
-			category: 'Makine',
-			tags: null,
+	let data = await prisma.post.findMany({
+		take: itemsPerPage,
+		skip: itemsToSkip,
+		where: {
+			OR: [
+				{
+					title: {
+						contains: query.id,
+					},
+				},
+				{
+					title: {
+						in: keywords,
+					},
+				},
+			],
+			AND: [
+				{
+					archived: false,
+				},
+			],
 		},
-		{
-			pid: 'cl9oes2af00008iwogax8m0qk',
-			created_at: '2022-10-25T16:14:59.150Z',
-			archived: false,
-			post_views: 1,
-			post_likes: 1,
-			title: 'Range Rover 2022',
-			description: 'Super mjet 1',
-			price: 1,
-			eur: true,
-			old_price: 100,
-			city: 'Tirana',
-			country: 'Albania',
-			category: 'Makine',
-			tags: null,
-		},
-		{
-			pid: 'cl9oes2af00008iwogax8m0qk',
-			created_at: '2022-10-25T16:14:59.150Z',
-			archived: false,
-			post_views: 1,
-			post_likes: 1,
-			title: 'Range Rover 2022',
-			description: 'Super mjet 1',
-			price: 1,
-			eur: true,
-			old_price: 100,
-			city: 'Tirana',
-			country: 'Albania',
-			category: 'Makine',
-			tags: null,
-		},
-		{
-			pid: 'cl9oes2af00008iwogax8m0qk',
-			created_at: '2022-10-25T16:14:59.150Z',
-			archived: false,
-			post_views: 1,
-			post_likes: 1,
-			title: 'Range Rover 2022',
-			description: 'Super mjet 1',
-			price: 1,
-			eur: true,
-			old_price: 100,
-			city: 'Tirana',
-			country: 'Albania',
-			category: 'Makine',
-			tags: null,
-		},
-		{
-			pid: 'cl9oes2af00008iwogax8m0qk',
-			created_at: '2022-10-25T16:14:59.150Z',
-			archived: false,
-			post_views: 1,
-			post_likes: 1,
-			title: 'Range Rover 2022',
-			description: 'Super mjet 1',
-			price: 1,
-			eur: true,
-			old_price: 100,
-			city: 'Tirana',
-			country: 'Albania',
-			category: 'Makine',
-			tags: null,
-		},
-		{
-			pid: 'cl9oes2af00008iwogax8m0qk',
-			created_at: '2022-10-25T16:14:59.150Z',
-			archived: false,
-			post_views: 1,
-			post_likes: 1,
-			title: 'Range Rover 2022',
-			description: 'Super mjet 1',
-			price: 1,
-			eur: true,
-			old_price: 100,
-			city: 'Tirana',
-			country: 'Albania',
-			category: 'Makine',
-			tags: null,
-		},
-		{
-			pid: 'cl9oes2af00008iwogax8m0qk',
-			created_at: '2022-10-25T16:14:59.150Z',
-			archived: false,
-			post_views: 1,
-			post_likes: 1,
-			title: 'Range Rover 2022',
-			description: 'Super mjet 1',
-			price: 1,
-			eur: true,
-			old_price: 100,
-			city: 'Tirana',
-			country: 'Albania',
-			category: 'Makine',
-			tags: null,
-		},
-	];
+	});
 
 	if (data) {
 		return new Response(JSON.stringify(data));
