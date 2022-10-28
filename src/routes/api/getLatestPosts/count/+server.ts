4012386bbb635fb316311cb5baed712f4b9ed211
Enes Bala <47826227+enesbala5@users.noreply.github.com>
2022-10-28 15:27:52 +0200

    Updated CountAPI with ErrorHandling - Hope it works

--- src/routes/api/getLatestPosts/count/+server.ts
+++ src/routes/api/getLatestPosts/count/+server.ts
@@ -2,12 +2,13 @@ import { error } from '@sveltejs/kit';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-/** @type {import('./$types').RequestHandler} */
+import type { RequestHandler } from './$types';
 
 // TODO:
 // -> Add other search options to query
-export async function POST({ request }) {
+export const POST: RequestHandler = async ({ request }) => {
 	const query = await request.json();
+	console.log('query count', query);
 
 	let count = await prisma.post.count({
 		where: {
@@ -26,5 +27,11 @@ export async function POST({ request }) {
 		},
 	});
 
-	return new Response(JSON.stringify(count));
-}
+	console.log('count', count);
+
+	if (count) {
+		return new Response(String(count));
+	}
+
+	throw error(404, 'Post Not Found');
+};
