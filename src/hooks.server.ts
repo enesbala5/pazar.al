fe8326662cdebf0d7f3fff836a5a7f9fbdc9b5b4
Enes Bala <47826227+enesbala5@users.noreply.github.com>
2022-11-02 11:29:15 +0100

    test hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -23,22 +23,31 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	console.log('there is session');
 
-	const user = await db.user.findUnique({
-		where: { userAuthToken: session },
-		select: {
-			name: true,
-			surname: true,
-			email: true,
-			role: true,
-		},
-	});
+	// const user = await db.user.findUnique({
+	// 	where: { userAuthToken: session },
+	// 	select: {
+	// 		name: true,
+	// 		surname: true,
+	// 		email: true,
+	// 		role: true,
+	// 	},
+	// });
 
-	if (user) {
+	// if (user) {
+	// 	event.locals.user = {
+	// 		name: user.name,
+	// 		surname: user.surname,
+	// 		email: user.email,
+	// 		role: user.role,
+	// 	};
+	// }
+
+	if (session) {
 		event.locals.user = {
-			name: user.name,
-			surname: user.surname,
-			email: user.email,
-			role: user.role,
+			name: 'Enes',
+			surname: 'Bala',
+			email: 'contact@enesbala.com',
+			role: 'USER',
 		};
 	}
 
