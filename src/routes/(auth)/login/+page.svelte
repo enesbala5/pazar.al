99e6b7416735b54125322b1940c5367a6e4e6dc9
Enes Bala <47826227+enesbala5@users.noreply.github.com>
2022-11-08 22:31:37 +0100

    changed login page to new design - should fix colors probably

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -3,52 +3,56 @@
 	import { applyAction, enhance } from '$app/forms';
 	import { invalidateAll } from '$app/navigation';
 	import { nav } from '$lib/userPreferences/nav';
-	import { Input } from 'postcss';
 	import InputField from '$lib/components/UI/Input/InputField.svelte';
 
 	export let form: ActionData;
 </script>
 
-<div class="mb-12 mt-12 p-4">
-	<h1 class=" text-4xl font-medium">Welcome Back</h1>
-	<p class="mt-2 opacity-80">Perdorni platformen E-Commerce me te re Shqiptare</p>
-</div>
-
-<form
-	method="POST"
-	class="px-4"
-	use:enhance={() => {
-		return async ({ result }) => {
-			invalidateAll();
-			await applyAction(result);
-		};
-	}}
->
-	<InputField classNames="mt-4" name="email" type="email" title="Email" required />
-	<InputField classNames="mt-4" name="password" type="password" title="Password" required />
-
-	{#if form?.invalid}
-		<p class=" mt-2 text-red-500">Email and password is required.</p>
-	{/if}
-
-	{#if form?.credentials}
-		<p class=" mt-2 text-red-500">You have entered the wrong credentials.</p>
-	{/if}
-
-	<div class="mt-4 flex items-center justify-between">
-		<div class="flex items-center space-x-2">
-			<input type="checkbox" value="rememberMe" />
-			<label for="rememberMe">Remember me</label>
+<div class="flex h-[80vh] flex-col items-center lg:justify-center pt-12 lg:pt-0">
+	<div class="flex w-full flex-col items-center px-4 lg:px-0">
+		<div class="mb-12 w-full max-w-md text-center">
+			<h1 class=" text-4xl font-medium">Hello</h1>
+			<p class="mt-2 opacity-80">
+				Sign in to Pazar.al or <a href={nav.register} class="underlinedLink">create an account</a>
+			</p>
 		</div>
-		<a href="forgotPassword" class="link">Forgot Password?</a>
-	</div>
 
-	<div class="mt-4">
-		<button type="submit" class="buttonPrimary w-full">Log in</button>
+		<form
+			method="POST"
+			class="w-full max-w-md"
+			use:enhance={() => {
+				return async ({ result }) => {
+					invalidateAll();
+					await applyAction(result);
+				};
+			}}
+		>
+			<InputField classNames="mt-4" name="email" type="email" title="Email" required />
+			<InputField classNames="mt-4" name="password" type="password" title="Password" required />
+
+			{#if form?.invalid}
+				<p class=" mt-2 text-red-500">Email and password is required.</p>
+			{/if}
+
+			{#if form?.credentials}
+				<p class=" mt-2 text-red-500">You have entered the wrong credentials.</p>
+			{/if}
+
+			<div class="mt-4 flex items-center justify-between">
+				<div class="flex items-center space-x-2">
+					<input type="checkbox" value="rememberMe" class="checkbox" />
+					<label for="rememberMe" class="labelText">Remember me</label>
+				</div>
+				<a href="forgotPassword" class="link labelText">Forgot Password?</a>
+			</div>
+
+			<div class="mt-4">
+				<button type="submit" class="buttonPrimary w-full">Log in</button>
+			</div>
+		</form>
+		
 	</div>
-</form>
-<div class="mt-4 flex w-full items-center justify-center">
-	<p>
-		Not registered yet? <a href={nav.register} class="underlinedLink">Create an Account</a>
-	</p>
+</div>
+<div class="fixed bottom-0 w-full border-t py-2 text-center dark:border-neutral-800">
+	<p class="tertiary">Copyright © 2022-2022 Pazar.al Shpk. All Rights Reserved.</p>
 </div>
