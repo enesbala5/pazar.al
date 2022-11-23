7bb707e463488e58f81e1af1db97018ac00f3ed9
Enes Bala <47826227+enesbala5@users.noreply.github.com>
2022-11-24 00:30:35 +0100

    Created Accordion Component -> Changes to Design
    LOT MORE WORK TO BE DONE

---
+++ src/lib/components/UI/Important/Accordion.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	export let visible: boolean = false;
+	import { slide } from 'svelte/transition';
+
+	export let showChevron: boolean = true;
+
+	export let title: string;
+
+	export const makeNotVisible = () => {
+		visible = false;
+	};
+
+	export const makeVisible = () => {
+		visible = true;
+	};
+
+	export const toggleVisible = () => {
+		visible = !visible;
+	};
+</script>
+
+<div
+	class=" my-4 rounded-xl border border-neutral-100 shadow-sm dark:shadow-none dark:border-neutral-800"
+	on:click={toggleVisible}
+	on:keydown={toggleVisible}
+>
+	<!-- H1 and Chevron -->
+	<div
+		class="flex w-full cursor-pointer items-center justify-between rounded-xl p-4 hover:bg-neutral-100  dark:hover:bg-neutral-800"
+	>
+		<h1 class=" text-xl font-medium">
+			{title}
+		</h1>
+
+		{#if showChevron}
+			<div class=" p-2">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="
+							{visible ? 'rotate-180 ' : ''}
+							h-3 w-3 fill-neutral-900 transition-transform ease-in-out dark:fill-neutral-100"
+					viewBox="0 0 320 191.9"
+				>
+					<path
+						id="Path_89"
+						data-name="Path 89"
+						d="M98.9,184.7l1.8,2.1,136,156.5a25.3,25.3,0,0,0,19.2,8.6,25.649,25.649,0,0,0,19.2-8.6L411,187.1l2.3-2.6a15.446,15.446,0,0,0,2.7-8.7c0-8.7-7.4-15.8-16.6-15.8H112.6c-9.2,0-16.6,7.1-16.6,15.8A15.27,15.27,0,0,0,98.9,184.7Z"
+						transform="translate(-96 -160)"
+					/>
+				</svg>
+			</div>
+		{/if}
+	</div>
+
+	<!-- Tags Management -->
+	{#if visible}
+		<div class=" flex flex-col space-y-4 rounded-xl p-4 " transition:slide={{ duration: 150 }}>
+			<slot />
+		</div>
+	{/if}
+</div>
--- src/routes/(alternate)/createPost/+page.svelte
+++ src/routes/(alternate)/createPost/+page.svelte
@@ -19,8 +19,8 @@
 	// Types
 	import type { Product, ProductTag } from '$lib/types/product';
 	import type { Selection } from '$lib/types/selection';
-	import { fade, fly, slide } from 'svelte/transition';
-	import { quadInOut } from 'svelte/easing';
+
+	import Accordion from '$lib/components/UI/Important/Accordion.svelte';
 
 	let eur: boolean = false;
 
@@ -74,8 +74,6 @@
 	function isRequired(value: any) {
 		return value != null && value !== '';
 	}
-
-	let errors = {};
 
 	const addRequiredTag = (event: any) => {
 		errors = {};
@@ -134,7 +132,7 @@
 
 	let focusInput: any, blurInput: any;
 
-	let showManageTags: boolean = false;
+	// let showManageTags: boolean = false;
 
 	let requiredTags: TagInCategory[] = [];
 
@@ -142,8 +140,11 @@
 		console.log('updating required');
 		requiredTags = getTagsByCategory(category?.value) ?? [];
 
-		showManageTags = true;
+		// showManageTags = true;
+		makeVisible();
 	}
+
+	let makeVisible: any;
 </script>
 
 <title>Krijo nje Postim - Pazar</title>
@@ -175,31 +176,37 @@
 						bind:value={price}
 					/>
 					<div
-						class="flex w-1/2  items-center rounded-full border border-neutral-200 fill-neutral-800 p-0.5 shadow-sm dark:border-neutral-800 dark:fill-neutral-200"
+						class="box-border flex w-1/2 items-center justify-between rounded-lg border border-neutral-300 pl-2.5  dark:border-neutral-600"
 					>
+						<p class="pointer-events-none text-sm text-neutral-500">Currency:</p>
+
 						<div
-							on:click={() => (eur = false)}
-							on:keydown={() => (eur = false)}
-							class="
+							class="inset-1 box-border flex w-2/3 items-center space-x-2 rounded-l-lg  border-l fill-neutral-800 py-0.5 px-2 shadow-sm dark:fill-neutral-200"
+						>
+							<div
+								on:click={() => (eur = false)}
+								on:keydown={() => (eur = false)}
+								class="
 							{!eur
-								? 'bg-indigo-600 fill-indigo-50 hover:bg-indigo-500'
-								: 'hover:bg-neutral-200 dark:hover:bg-neutral-800'}
-							flex h-full w-full items-center justify-center rounded-full py-2 "
-						>
-							<Lek classNames="w-6 h-6" />
-						</div>
-						<div
-							on:click={() => (eur = true)}
-							on:keydown={() => (eur = true)}
-							class="
+									? 'border-indigo-600 bg-indigo-600 fill-indigo-50 hover:border-indigo-500 hover:bg-indigo-500'
+									: 'border-neutral-300 bg-neutral-100 hover:bg-neutral-200  dark:border-neutral-600 dark:hover:bg-neutral-800'}
+							my-1 box-border flex h-full w-full items-center justify-center overflow-hidden rounded-lg border py-1.5"
+							>
+								<Lek classNames="w-7 h-5" />
+							</div>
+							<div
+								on:click={() => (eur = true)}
+								on:keydown={() => (eur = true)}
+								class="
 							{eur
-								? 'bg-indigo-600 fill-indigo-50 hover:bg-indigo-500'
-								: 'hover:bg-neutral-200 dark:hover:bg-neutral-800'}
-							flex h-full w-full items-center justify-center rounded-full py-2"
-						>
-							<!-- surrounding div because it was 1 unit bigger than the Lek - this is to make it equal -->
-							<div class="p-0.5">
-								<Euro classNames="w-5 h-5" />
+									? 'border-indigo-600 bg-indigo-600 fill-indigo-50 hover:border-indigo-500 hover:bg-indigo-500'
+									: 'border-neutral-300 bg-neutral-100 hover:bg-neutral-200  dark:border-neutral-600 dark:hover:bg-neutral-800'}
+							my-1 box-border flex h-full w-full items-center justify-center rounded-lg border py-1.5"
+							>
+								<!-- surrounding div because it was 1 unit bigger than the Lek - this is to make it equal -->
+								<div class="">
+									<Euro classNames="w-5 h-5" />
+								</div>
 							</div>
 						</div>
 					</div>
@@ -224,165 +231,113 @@
 				</div>
 				<hr class="my-4 border-neutral-100 dark:border-neutral-800" />
 
-				<div
-					class=" my-4 rounded-xl bg-neutral-100 p-4 shadow-sm dark:border-neutral-800  dark:bg-neutral-800"
-				>
-					<!-- H1 and Chevron -->
-					<div class="flex w-full items-center justify-between">
-						<h1
-							class="cursor-pointer text-xl font-medium hover:underline"
-							on:click={() => (showManageTags = !showManageTags)}
-							on:keydown={() => (showManageTags = !showManageTags)}
-						>
-							Manage Tags
-						</h1>
+				<Accordion title="Manage Tags" bind:makeVisible>
+					<!-- ? Inserting Tags -->
+					<div class=" ">
+						<p class="mb-2 text-sm font-medium">* Based on Category:</p>
+						<hr class="my-2 border-neutral-200 dark:border-neutral-800" />
 
-						<div
-							class="cursor-pointer p-2"
-							on:click={() => (showManageTags = !showManageTags)}
-							on:keydown={() => (showManageTags = !showManageTags)}
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="
-							{showManageTags ? 'rotate-180 ' : ''}
-							h-3 w-3 fill-neutral-900 transition-transform ease-in-out dark:fill-neutral-100"
-								viewBox="0 0 320 191.9"
-							>
-								<path
-									id="Path_89"
-									data-name="Path 89"
-									d="M98.9,184.7l1.8,2.1,136,156.5a25.3,25.3,0,0,0,19.2,8.6,25.649,25.649,0,0,0,19.2-8.6L411,187.1l2.3-2.6a15.446,15.446,0,0,0,2.7-8.7c0-8.7-7.4-15.8-16.6-15.8H112.6c-9.2,0-16.6,7.1-16.6,15.8A15.27,15.27,0,0,0,98.9,184.7Z"
-									transform="translate(-96 -160)"
-								/>
-							</svg>
-						</div>
-					</div>
-
-					<!-- Tags Management -->
-					{#if showManageTags}
-						<div
-							class="mt-4 flex  flex-col space-y-4 rounded-xl "
-							transition:slide={{ duration: 150 }}
-						>
-							<!-- ? Inserting Tags -->
-							<div class=" ">
-								<p class="mb-2 text-sm font-medium">* Based on Category:</p>
-								<hr class="my-2 border-neutral-200 dark:border-neutral-800" />
-
-								{#if requiredTags.length > 0}
-									{#each requiredTags as requiredTag}
-										<form on:submit|preventDefault={addRequiredTag} class="my-4 ">
-											<div class="flex items-center justify-center space-x-4">
-												<div class="w-full">
-													<input
-														type="hidden"
-														value={requiredTag.name}
-														name="requiredTagText"
-														class="hidden"
-													/>
-													<InputField
-														name="Placeholder"
-														title="Tag Name"
-														type="text"
-														value={requiredTag.name}
-														disabled
-													/>
-												</div>
-												<div class="w-full">
-													{#if requiredTag.options}
-														<input
-															type="hidden"
-															value={true}
-															name="isSelectOption"
-															class="hidden"
-														/>
-														<Select
-															items={requiredTag.options}
-															justValue
-															name="requiredTagValue"
-															placeholder="{requiredTag.name} Options"
-														/>
-													{:else}
-														<InputField
-															title="Tag Value"
-															type="text"
-															name="requiredTagValue"
-															bind:value={tagValue}
-														/>
-													{/if}
-												</div>
-											</div>
-											<button class=" buttonSmall mt-2 whitespace-nowrap">Add Tag</button>
-										</form>
-									{/each}
-								{/if}
-								<form on:submit|preventDefault={addTag} class=" my-4">
+						{#if requiredTags.length > 0}
+							{#each requiredTags as requiredTag}
+								<form on:submit|preventDefault={addRequiredTag} class="my-4 ">
 									<div class="flex items-center justify-center space-x-4">
 										<div class="w-full">
+											<input
+												type="hidden"
+												value={requiredTag.name}
+												name="requiredTagText"
+												class="hidden"
+											/>
 											<InputField
+												name="Placeholder"
 												title="Tag Name"
 												type="text"
-												name="tagText"
-												bind:value={tagText}
-												bind:blur={blurInput}
-												bind:focus={focusInput}
+												value={requiredTag.name}
+												disabled
 											/>
 										</div>
 										<div class="w-full">
-											<InputField
-												title="Tag Value"
-												type="text"
-												name="tagValue"
-												bind:value={tagValue}
-											/>
+											{#if requiredTag.options}
+												<input type="hidden" value={true} name="isSelectOption" class="hidden" />
+												<Select
+													items={requiredTag.options}
+													justValue
+													name="requiredTagValue"
+													placeholder="{requiredTag.name} Options"
+												/>
+											{:else}
+												<InputField
+													title="Tag Value"
+													type="text"
+													name="requiredTagValue"
+													bind:value={tagValue}
+												/>
+											{/if}
 										</div>
 									</div>
 									<button class=" buttonSmall mt-2 whitespace-nowrap">Add Tag</button>
 								</form>
-							</div>
-							<!-- ? end -->
-
-							<!-- * Editing Tags -->
-							<div class="">
-								<p class="mb-2 text-sm font-medium">My Tags:</p>
-								<hr class="my-2 mb-4 border-neutral-200 dark:border-neutral-800" />
-
-								<div class="flex items-center ">
-									{#each tags as tag}
-										<div class="w-full">
-											<InputField
-												title="Tag Name"
-												type="text"
-												name="tagText"
-												bind:value={tag.name}
-												bind:blur={blurInput}
-												bind:focus={focusInput}
-											/>
-										</div>
-										<div class="w-full">
-											<InputField
-												title="Tag Value"
-												type="text"
-												name="tagValue"
-												bind:value={tag.value}
-											/>
-										</div>
-										<button on:click={() => deleteTag(tag.id)} class="buttonSmall whitespace-nowrap"
-											>Delete Tag</button
-										>
-									{:else}
-										<div class="text-sm ">
-											<p>You have not added any Tags yet.</p>
-										</div>
-									{/each}
+							{/each}
+						{/if}
+						<form on:submit|preventDefault={addTag} class=" my-4">
+							<div class="flex items-center justify-center space-x-4">
+								<div class="w-full">
+									<InputField
+										title="Tag Name"
+										type="text"
+										name="tagText"
+										bind:value={tagText}
+										bind:blur={blurInput}
+										bind:focus={focusInput}
+									/>
 								</div>
-
-								<!-- * end -->
+								<div class="w-full">
+									<InputField title="Tag Value" type="text" name="tagValue" bind:value={tagValue} />
+								</div>
 							</div>
+							<button class=" buttonSmall mt-2 whitespace-nowrap">Add Tag</button>
+						</form>
+					</div>
+					<!-- ? end -->
+
+					<!-- * Editing Tags -->
+					<div class="">
+						<p class="mb-2 text-sm font-medium">My Tags:</p>
+						<hr class="my-2 mb-4 border-neutral-200 dark:border-neutral-800" />
+
+						<div class="flex items-center ">
+							{#each tags as tag}
+								<div class="w-full">
+									<InputField
+										title="Tag Name"
+										type="text"
+										name="tagText"
+										bind:value={tag.name}
+										bind:blur={blurInput}
+										bind:focus={focusInput}
+									/>
+								</div>
+								<div class="w-full">
+									<InputField
+										title="Tag Value"
+										type="text"
+										name="tagValue"
+										bind:value={tag.value}
+									/>
+								</div>
+								<button on:click={() => deleteTag(tag.id)} class="buttonSmall whitespace-nowrap"
+									>Delete Tag</button
+								>
+							{:else}
+								<div class="text-sm ">
+									<p>You have not added any Tags yet.</p>
+								</div>
+							{/each}
 						</div>
-					{/if}
-				</div>
+
+						<!-- * end -->
+					</div>
+				</Accordion>
 			</form>
 			<div>
 				<div class="" />
@@ -401,13 +356,26 @@
 					message="You can edit your post even after uploading&nbsp;it."
 				/>
 			</div>
+
 			<div class="mt-4">
 				<p class="font-medium">Post Tags</p>
-				<div class="mt-2 flex flex-wrap ">
-					{#each tags as tag, i}
-						<Badge margin title={`${tag.name}: `} message={tag.value} />
-					{/each}
-				</div>
+				{#if tags.length > 0}
+					<div class="mt-2 flex flex-wrap ">
+						{#each tags as tag, i}
+							<Badge margin title={`${tag.name}: `} message={tag.value} />
+						{/each}
+					</div>
+				{/if}
+			</div>
+			<hr class="my-4 border-neutral-100 dark:border-neutral-800" />
+
+			<div class="mt-4">
+				<p class="font-medium">Location Preview</p>
+			</div>
+			<hr class="my-4 border-neutral-100 dark:border-neutral-800" />
+
+			<div class="mt-4">
+				<p class="font-medium">Account Preview</p>
 			</div>
 		</section>
 	</article>
